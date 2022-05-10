359e384014d4be5c71f151e01f8ad114a8936e43
gustavolopesk <gustavokkramer@gmail.com>
2022-05-10 15:19:06 -0300

    Projeto criado

---
+++ backend/src/main/java/com/devgustavo/ftvideos/FtvideosApplication.java
@@ -0,0 +1,13 @@
+package com.devgustavo.ftvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtvideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtvideosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devgustavo/ftvideos/FtvideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devgustavo.ftvideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtvideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
